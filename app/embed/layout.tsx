6c506ef7066f98dfeb50bd251cc780f832173736
exposuremedia <mike@exposuremedia.com>
2026-09-10 13:04:18 -0400

    Add app/embed

---
+++ app/embed/layout.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+export const metadata = {
+  title: "Labor Force Link",
+};
+
+export default function EmbedLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
